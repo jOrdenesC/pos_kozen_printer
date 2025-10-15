e35a398005a1860580cbfbe3d5fd6999dd33d00a
Javier Órdenes <45943743+jOrdenesC@users.noreply.github.com>
2025-10-15 13:07:06 -0300

    Update example app for v0.0.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pos_kozen_printer/pos_kozen_printer.dart';
